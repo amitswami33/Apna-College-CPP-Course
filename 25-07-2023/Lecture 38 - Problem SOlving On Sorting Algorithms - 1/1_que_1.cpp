9f05568368aea21c97907b518484367575d95718
amitswami33 <amitswami3333@gmail.com>
2023-07-29 23:06:51 +0530

    Day 12

---
+++ 25-07-2023/Lecture 38 - Problem SOlving On Sorting Algorithms - 1/1_que_1.cpp
@@ -0,0 +1,2 @@
+//Given an integer arr, move all 0's to the end of it while maintaining the relative order of the nonzero elements. Note that you must do this in-place without making a copy of array.
+// input : 0 5 0 3 42 ... output : 5 3 42 0 0
